964eac626c5bc16d740974018ff641ecf904287c
mike <mike@nova.(none)>
2013-03-13 14:04:52 +0000

    handle rexfile errors, display error message to browser

--- lib/Rex/WebUI/Project.pm
+++ lib/Rex/WebUI/Project.pm
@@ -10,18 +10,24 @@ sub index {
    my $id = $self->param("id");
    my $project = $self->config->{projects}->[$id];
 
-   $self->rex->load_rexfile($project->{rexfile});
+   if ($self->rex->load_rexfile($project->{rexfile})) {
 
-   my $tasks   = $self->rex->get_tasks;
-   my $servers = $self->rex->get_servers;
+	   my $tasks   = $self->rex->get_tasks;
+	   my $servers = $self->rex->get_servers;
 
-   $self->stash(name => $project->{name});
+	   $self->stash(name => $project->{name});
 
-   $self->stash(rexfile => $self->rex->{rexfile});
-   $self->stash(tasks => $tasks);
-   $self->stash(servers => $servers);
+	   $self->stash(rexfile => $self->rex->{rexfile});
+	   $self->stash(tasks => $tasks);
+	   $self->stash(servers => $servers);
 
-   $self->render;
+	   $self->render;
+   }
+   else {
+
+	   $self->stash(error => $self->rex->error_message);
+	   $self->render(template => 'project/rexfile_error');
+   }
 }
 
 1;
